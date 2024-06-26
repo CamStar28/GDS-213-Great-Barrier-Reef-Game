e396541b77cb702ab96d2854b1891a993e497f66
CamStar28 <129808323+CamStar28@users.noreply.github.com>
2024-06-26 16:39:54 +1000

    Turtle Added

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,8 +6,10 @@ public class cameraController : MonoBehaviour
 {
 
     public GameObject potatoCodImage;
+    public GameObject greenTurtleImage;
 
     public GameObject potatoCodBio; 
+    public GameObject greenTurtleBio;
     
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,11 @@ public class cameraController : MonoBehaviour
             potatoCodImage.SetActive(true);
             Debug.Log("fish wit da LIPS");
             potatoCodBio.gameObject.GetComponent<fishBioController>().UnlockBio(); 
+        } else if (other.gameObject.tag == "GreenTurtle")
+        {
+            greenTurtleImage.SetActive(true);
+            Debug.Log("I spent hours of my life stomping... KOOPAS");
+            greenTurtleBio.gameObject.GetComponent<fishBioController>().UnlockBio();
         }
     }
 }
