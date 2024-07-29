e5d94b9a8f8c8dcd960b049bf3f2928fd0d40c94
CamStar28 <dalycameron28@gmail.com>
2024-07-29 16:45:21 +1000

    Boids!

--- Assets/Scripts/Fish Scripts/whaleSharkBehaviour.cs
+++ Assets/Scripts/Fish Scripts/whaleSharkBehaviour.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding; 
 
 public class whaleSharkBehaviour : MonoBehaviour
 {
-    private AIPath path;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private Transform target;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        path = GetComponent<AIPath>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        path.maxSpeed = moveSpeed;
-
-        path.destination = target.position;
+        
     }
 }
